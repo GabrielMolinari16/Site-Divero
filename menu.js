dd1aec642e4bf347e4e6e005381f2e615a69c69b
GabrielMolinari16 <gabrielmolinari.2003@gmail.com>
2024-09-24 08:20:58 -0300

    alterações feitas por matheus

--- menu.js
+++ menu.js
@@ -13,47 +13,24 @@ menuItem.forEach((item) =>
 
 // expandindo pelo botao de expandir
 
-var btnExp = document.querySelector('#btnExp');
-var ladoMenu = document.querySelector('.menu-lateral');
-var logo = document.querySelector('.logo-hidden');
+// var btnExp = document.querySelector('#btnExp');
+// var ladoMenu = document.querySelector('.menu-lateral');
+// var logo = document.querySelector('.logo-hidden');
 
-btnExp.addEventListener('click', function(){
-    ladoMenu.classList.toggle('expand');
-    // faz a logo aparecer e desaparecer quando colica para expandir o btnExp
-    logo.classList.toggle('logo-divero');
-    logo.classList.toggle('logo-hidden');
-})
+// btnExp.addEventListener('click', function(){
+//     ladoMenu.classList.toggle('expand');
+//     // faz a logo aparecer e desaparecer quando colica para expandir o btnExp
+//     logo.classList.toggle('logo-divero');
+//     logo.classList.toggle('logo-hidden');
+// })
 
-// fazendo aparecer os links de cada modelagem
+// testes
 
-// const grupo = document.querySelectorAll('.grupo');
-// const subgrupo = document.querySelectorAll('.subgrupo');
-// const tipo = document.querySelectorAll('.tipo');
+// var menu = document.querySelector('nav.menu-lateral');
 
-// function abreFechaGruposMenu(){
-//     grupo.forEach(item => 
-//         item.style.display = 'none'
-//     );
-//     this.querySelectorAll('.grupo').forEach(item =>
-//         item.style.display = 'block'
-//     );
-// }
-
-// function abreFechaSubgruposMenu(){
-//     subgrupo.forEach(item => 
-//         item.style.display = 'none'
-//     );
-//     this.querySelectorAll('.subgrupo').forEach(item =>
-//         item.style.display = 'block'
-//     );
-// }
-
-// menuItem.forEach((item) =>
-//     item.addEventListener('click', abreFechaGruposMenu, {passive: true})
-// );
-
-// grupo.forEach((item) =>
-//     item.addEventListener('click', abreFechaSubgruposMenu, {passive: true})
-// );
-
-// element.addEventListener('touchstart', handlerFunction, { passive: true });
+// menu.addEventListener('mouseover', function() {
+//     console.log('Mouse entrou no menu!');
+//     // ladoMenu.classList.toggle('expand');
+//     // faz a logo aparecer e desaparecer quando colica para expandir o btnExp
+   
+// });
